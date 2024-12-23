257d2850aea83b6c745ca79c67f3707b4eecb8a6
myeganeh2876 <mamady83@gmail.com>
2024-12-23 13:28:19 +0330

    init

---
+++ MDatePickerDemo/MDatePickerDemo/MDatePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDatePickerDemoApp.swift
+//  MDatePickerDemo
+//
+//  Created by mohammad yeganeh on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MDatePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
