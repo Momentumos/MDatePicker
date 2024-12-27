dbc59fa5eebf2e29dfa34f7705c683108a40981f
myeganeh2876 <mamady83@gmail.com>
2024-12-27 12:35:06 +0330

    added value accessors to MPickedDate

--- Sources/MDatePicker/MPickedDate.swift
+++ Sources/MDatePicker/MPickedDate.swift
@@ -7,11 +7,30 @@
 
 import Foundation
 
-public enum MPickedDate: Equatable {
+public enum MPickedDate: Equatable, Codable, Hashable, Sendable {
 
     case single(Date)
     case range(Date, Date)
+    
+    
+    public var singleValue: Date {
+        switch self {
+        case .single(let date):
+            return date
+        case .range(let start, let end):
+            return start
+        }
+    }
 
+    public var rangeValue: ClosedRange<Date> {
+        switch self {
+        case .single(let date):
+            return .init(uncheckedBounds: (date, date))
+        case .range(let start, let end):
+            return .init(uncheckedBounds: (start, end))
+        }
+    }
+    
     public static func == (lhs: Self, rhs: Self) -> Bool {
         switch (lhs, rhs) {
         case let (.single(l), .single(r)):
@@ -26,11 +45,27 @@ public enum MPickedDate: Equatable {
     public var description: String {
         let formatter = DateFormatter()
         formatter.setLocalizedDateFormatFromTemplate("MMMM dd")
+        
+        let calendar = Calendar.current
+        let today = Date()
+        
+        func formatDate(_ date: Date) -> String {
+            if calendar.isDateInToday(date) {
+                return "Today"
+            } else if calendar.isDateInTomorrow(date) {
+                return "Tomorrow"
+            } else if calendar.isDateInYesterday(date) {
+                return "Yesterday"
+            } else {
+                return formatter.string(from: date)
+            }
+        }
+        
         switch self {
         case let .single(date):
-            return formatter.string(from: date)
+            return formatDate(date)
         case let .range(date, date2):
-            return "\(formatter.string(from: date)) - \(formatter.string(from: date2))"
+            return "\(formatDate(date)) - \(formatDate(date2))"
         }
     }
 }
